a0242b704c325ee0fb1e44e1845722f9d17d6c3f
wandalen <wandalen.me@gmail.com>
2017-10-15 21:42:27 +0400

    adjust sample

--- sample/Sample.js
+++ sample/Sample.js
@@ -3,7 +3,7 @@ if( typeof module !== 'undefined' )
 require( 'wtemplatetreeresolver' );
 
 var tree = { 'a' : 'a', b : [ 1,2,3 ], c : { c1 : [ 1,2,3 ], c2 : [ 11,22,33 ] }, d : '{{^^a}}' }
-var templateTree = new wTemplateTree();
+var templateTree = new wTemplateTreeResolver();
 templateTree.tree = tree;
 
 var b1 = templateTree.query( 'b/1' );
