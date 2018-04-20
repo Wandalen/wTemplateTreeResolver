10f3670df69844e711ea91e47d17a0026e506789
Wandalen <wandalen.me@gmail.com>
2018-04-20 11:22:44 +0300

    sync

--- staging/dwtools/amid/mapping.test/TemplateTree.test.s
+++ staging/dwtools/amid/mapping.test/TemplateTree.test.s
@@ -7,7 +7,7 @@ if( typeof module !== 'undefined' )
 
   if( typeof _global_ === 'undefined' || !_global_.wBase )
   {
-    let toolsPath = '../../../../dwtools/Base.s';
+    let toolsPath = '../../../dwtools/Base.s';
     let toolsExternal = 0;
     try
     {
@@ -22,10 +22,10 @@ if( typeof module !== 'undefined' )
     require( toolsPath );
   }
 
-var _ = _global_.wTools;
+  var _ = _global_.wTools;
 
   _.include( 'wTesting' );
-  require( '../mapping/TemplateTreeAresolver.s' );
+  require( '../amapping/TemplateTreeAresolver.s' );
 
 }
 
