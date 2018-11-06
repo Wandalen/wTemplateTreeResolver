7d964f0487b51ad1345a417bfbe322d90021952a
Wandalen <wandalen.me@gmail.com>
2018-11-06 17:03:49 +0200

    auto

--- proto/dwtools/amid/amapping.test/TemplateTree.test.s
+++ proto/dwtools/amid/amapping.test/TemplateTree.test.s
@@ -16,7 +16,7 @@ if( typeof module !== 'undefined' )
 //
 
 var _ = _global_.wTools;
-var Parent = /*_.*/wTester;
+var Parent = wTester;
 
 var tree =
 {
@@ -628,6 +628,6 @@ var Self =
 
 Self = wTestSuite( Self );
 if( typeof module !== 'undefined' && !module.parent )
-/*_.*/wTester.test( Self.name );
+wTester.test( Self.name );
 
 } )( );
