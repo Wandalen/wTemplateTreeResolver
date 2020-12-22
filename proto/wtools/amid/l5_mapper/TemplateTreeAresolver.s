5cdcd254029d3453c70e2e075ed507d5a9141731
Dmytro <dm.vict.kr@gmail.com>
2020-12-22 12:47:57 +0200

    Fix style of files

--- proto/wtools/amid/l5_mapper/TemplateTreeAresolver.s
+++ proto/wtools/amid/l5_mapper/TemplateTreeAresolver.s
@@ -1,4 +1,5 @@
-( function _TemplateTreeAresolver_s_( ) {
+( function _TemplateTreeAresolver_s_()
+{
 
 'use strict';
 
@@ -260,7 +261,7 @@ function _resolveString( src )
 
   let o2 =
   {
-    src : src,
+    src,
     prefix : self.prefixToken,
     postfix : self.postfixToken,
     onInlined : function( src ){ return [ src ]; },
@@ -312,8 +313,8 @@ function _resolveString( src )
         debugger;
         element2 = _.err
         (
-         'Cant resolve', _.strQuote( src.substring( 0, 80 ) ), '\n', _.strQuote( strip ), 'is', _.strType( element2 ), '\n',
-         'Allowed types are: String, Array, Map'
+          'Cant resolve', _.strQuote( src.substring( 0, 80 ) ), '\n', _.strQuote( strip ), 'is', _.strType( element2 ), '\n',
+          'Allowed types are: String, Array, Map'
         );
         return element2;
       }
@@ -781,7 +782,7 @@ function EntityResolve( src, tree )
   if( tree === undefined )
   tree = src;
   _.assert( arguments.length === 1 || arguments.length === 2 );
-  let self = new Self({ tree : tree });
+  let self = new Self({ tree });
   return self.resolve( src );
 }
 
@@ -830,7 +831,7 @@ let Composes =
   upToken : _.define.own([ '\\/', '/' ]),
 
   onStrFrom : null,
-  onUpTokenDefault : onUpTokenDefault,
+  onUpTokenDefault,
 
 }
 
